58e4dccb7c9fefdc3eb97d2a5a6564124634481f
Patricia Correia <pncorreia25@gmail.com>
2023-03-20 23:15:46 -0300

    scrape pesquisando titulos

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,10 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    search = search_news({"title": {"$regex": title, "$options": "$i"}})
+    return [(news["title"], news["url"]) for news in search]
 
 
 # Requisito 8
